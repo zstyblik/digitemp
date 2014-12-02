646940800eaeebe804eba1faff69c768f4c73b85
Zdenek Styblik <stybla@turnovfree.net>
2014-12-02 15:58:10 +0100

    Re-work config-serial.sh - it's not like it matters or anything

--- shell/config-serial.sh
+++ shell/config-serial.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
+# Desc: Setup given serial device according to params specified in given config
 set -e
 set -u
-CONFIG=${CONFIG:-'/root/scripts/digitemp/config-serial.conf'}
-SERIALDEV=${SERIALDEV:-'/dev/ttyUSB0'}
-STTY=/usr/bin/stty
 
-if [ ! -c "${SERIALDEV}" ]; then
-	echo "File '${SERIALDEV}' doesn't seem to be char device. Quitting."
-	exit 1;
+SERIALDEV=${1:-''}
+CONFIG=${2:-''}
+if [ -z "${SERIALDEV}" ] || [ -z "${CONFIG}" ]; then
+	printf -- "Usage: %s <serial_dev> <serial_config>" "${0}" 1>&2
+	exit 1
 fi
 
-if [ ! -e "${CONFIG}" ]; then
-	echo "Config '${CONFIG}' doesn't seem to exist. Quitting."
-	exit 1;
-fi
-
-if [ ! -x "${STTY}" ]; then
-	echo "'${STTY}' doesn't exist nor executable."
-	exit 1;
-fi
-
-for PARAM in $(cat "${CONFIG}" | sed -e 's# #_#g'); do
-	 $STTY -F ${SERIALDEV} $(echo ${PARAM} | sed -e  's#_# #g');
+IFS='
+'
+for PARAM in $(cat -- "${CONFIG}"); do
+	/usr/bin/stty -F "${SERIALDEV}" ${PARAM}
 done
